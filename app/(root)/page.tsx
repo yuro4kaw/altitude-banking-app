1e9223d1a0166779e6df35ad8ae3d328705bf2ec
Yurii Matvii <tahkict.yup4ik@gmail.com>
2024-05-09 15:06:58 +0300

    feature: home title and navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = { firstName: "Yurii" };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-export default Home
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={5521.70}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+      <CountUp 
+        end={amount} 
+        decimal=","
+        prefix="$"
+        decimals={2}
+       />
+    </div>
+  );
+};
+
+export default AnimatedCounter;
