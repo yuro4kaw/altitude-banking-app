f6932ff1fc2a0d7b2019499d581d51353c88fc9d
Yurii Matvii <tahkict.yup4ik@gmail.com>
2024-05-11 01:34:17 +0300

    feature: auth page UI and forms

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    // Mutation / Database / Make feth
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signUp = async (userData:SignUpParams) => {
+  try {
+    // Mutation / Database / Make feth
+  } catch (error) {
+    console.log(error);
+  }
+};
