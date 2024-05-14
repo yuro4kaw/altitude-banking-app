1b54f419d4b019c0e3175310ceb862e84152780e
Yurii <tahkict.yup4ik@gmail.com>
2024-05-14 03:18:24 +0300

    refactor

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
           {children}
           <div className="auth-asset">
             <div>
-              <Image src='/icons/auth-image.svg' alt="Auth Image" width={500} height={500} />
+              <Image src='/icons/auth-img.svg' alt="Auth Image" width={500} height={500} />
             </div>
           </div>
       </main>
